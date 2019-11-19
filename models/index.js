11ecb657db0d0167c29f80565e98ddfe8da0259d
Jessie Jones <Jessie.Jones.Jr@gmail.com>
2019-11-19 13:44:49 -0700

    fleshing out the front end

--- models/index.js
+++ models/index.js
@@ -5,12 +5,17 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.JAWSDB_URL || "development";
-// var config = require(__dirname + "/../config/config.json")[env];
+var config = require(__dirname + "/../config/config.json")[env]; //dev only
 var db = {};
 
 if (process.env.JAWSDB_URL) {
   var sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+}
+else if (config.use_env_variable) {
+
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  
+}else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
