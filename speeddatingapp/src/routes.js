01a21490ea6925a37697fad469954d8a660c3e0c
Jessie Jones <Jessie.Jones.Jr@gmail.com>
2019-11-30 17:27:06 -0700

    routes done I think

--- speeddatingapp/src/routes.js
+++ speeddatingapp/src/routes.js
@@ -8,7 +8,55 @@ import Chat from './components/views/chat';
 
 
 
+export default (
+  <Route path='/' component={App}>
+    <IndexRoute component={Login} />
+    <Route path='/Login' component={Login} />
+    <Route path='/waitingRoom/success' component={Room} />
+    <Route path='/game/:gameID' component={Chat} />
+    <Route path='*' component={Home} />
+  </Route>
+);
 
+// const path = require("path");
+// const restrict = require("./../config/middleware/isAuthenticated");
+// // app.get('/route-with-restricted-access', restrict, function(res, res, next) {
+// //   // Handle request...
+// // });
+// // http://toon.io/understanding-passportjs-authentication-flow/
+// // http://toon.io/on-passportjs-specific-use-cases/
+// module.exports = function (app) {
+
+//   app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../public/login.html"));
+
+//   });
+
+//   app.get("/Login", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../public/login.html"));
+//   });
+//   // app.get("/WaitingRoom", function(req, res) {
+//   //   res.sendFile(path.join(__dirname, "../public/login.html"));
+//   // });
+
+
+//   // app.get("/login/incorrect_password", function(req, res) {
+//   //   res.sendFile(path.join(__dirname, "../public/incorrect.html"));
+//   // });
+
+//   app.get("/waitingroom/success", restrict, function (req, res, next) {
+//     res.sendFile(path.join(__dirname, "../public/waitRoom.html"));
+
+//   });
+
+//   app.get("/game/:gameID", restrict, function (req, res) {
+//     console.log("game redirect")
+//     res.sendFile(path.join(__dirname, "../public/gameroom.html"));
+//   });
+
+
+
+// };
 
 // const db = require("../models");
 // const passport = require("passport");
