75941f20ef197007708cef10c345b948633836ca
Jessie Jones <Jessie.Jones.Jr@gmail.com>
2019-12-01 23:52:57 -0700

    done for today

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -4,25 +4,57 @@ import Button from "react-bootstrap/Button";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./../../stylesheets/global.css";
 import MyVerticallyCenteredModal from "./../myVerticallyCenteredMondal";
+import axios from 'axios';
 
 
-export default function Login() {
-  const [modalShow, setModalShow] = React.useState(false);
+
+
+class Login extends React.Component {
+   constructor(){
+     super()
+     this.state={
+       email: "",
+       password: ""
+     }
+   }
+   handleChangeEmail = event => {
+    this.setState({ email: event.target.value });
+  }
+  handleChangePassword = event => {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios.post(`/login`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+ 
   
 
-
-     return (
+   render(){
+    const [modalShow, setModalShow] = React.useState(false);
+    return (
       <>
          <div id="titleCard"></div>
       
          <div id="loginBox">
-             <form action="/login" method="POST" id="loginBoxForm">
+             <form onSubmit={this.handleSubmit} id="loginBoxForm">
                  <h1 id="loginTitle">Speed Dater Login</h1>
-                 <input className="input1" name2="email" type="email" id="email" name="email" placeholder="Email"/>
+                 <input className="input1" name2="email" type="email" id="email" name="email" placeholder="Email" onChange={this.handleChangeEmail}/>
                  <br></br>
-                 <input className="input1" name2="password" type="password" id="password" name="password" placeholder="Password"/>
+                 <input className="input1" name2="password" type="password" id="password" name="password" placeholder="Password"onChange={this.handleChangePassword}/>
                  <br></br>
-                 <button id="loginSubmit" >Log In</button>
+                 <button type="submit" id="loginSubmit" >Log In</button>
                  <br></br>
   <ButtonToolbar>
       <Button id="newUser" onClick={() => setModalShow(true)}>
@@ -51,4 +83,6 @@ export default function Login() {
      </div>
     </>
      );
+   }
+    
    }
