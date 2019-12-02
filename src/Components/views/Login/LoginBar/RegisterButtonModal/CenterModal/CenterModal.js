00fd4beb55d4bccf6c2e5ef5c17cd9080ff32ad1
Jessie Jones <Jessie.Jones.Jr@gmail.com>
2019-12-02 15:38:28 -0700

    still breaking down components for login to work...need to get an object from state on submit

--- src/Components/views/Login/LoginBar/RegisterButtonModal/CenterModal/CenterModal.js
+++ src/Components/views/Login/LoginBar/RegisterButtonModal/CenterModal/CenterModal.js
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
   import "./styles_CenterModalComponent.css"
 
   // Local Dependencies
-
+import RegisterForm from "./RegisterForm/RegisterFrom"
 
 
 
@@ -31,36 +31,7 @@ export default function CenterModal(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-      <div id="newUserEntry">
-
-                
-              <div id="form_entry">
-                <p>First Name:</p>
-                <br></br> 
-                <input required className="input" pattern=".[A-Za-z]{2,20}" type="text" name2="firstName" id="firstName_newUser" title="Must contain at least two characters A-Z"/>
-                <p>Last Name: </p> 
-                <br></br> 
-                <input required className="input" pattern=".[A-Za-z]{2,20}" type="text" name2="lastName"id="lastName_newUser" title="Must contain at least two characters A-Z"/>
-      
-                <p>Email (Username): </p> 
-                <br></br> 
-                <input required className="input" type="email" name2="email2" id="email_newUser" placeholder="h@h.com"/>
-      
-                <p>Password:</p> 
-                <br></br>
-                <input required className="input" pattern=".{6,20}" type="password" name2="password" id="newPassword1" title="Must contain at least 6 characters A-Z"/>
-      
-                <p>Confirm Password:</p> 
-                <br></br>
-                <input required className="input" pattern=".{6,20}" type="password" name2="confirm" id="newPassword2"/>
-      
-      
-                <button id="registerSubmit">Submit</button>
-              </div>
-              <div id="messageBox">
-                <p id="passwordError"></p>
-            </div>
-        </div>
+     <RegisterForm/>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
